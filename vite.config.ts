a4312afffd7dc262cc63c36355e38d648714120c
dinhdang221101 <82924951+dinhdang221101@users.noreply.github.com>
2025-02-08 12:31:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '35.160.120.126', 
+    host: '0.0.0.0', 
     port: 5173 
   }
 })
