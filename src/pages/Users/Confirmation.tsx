a9725552c8996ec47cfd09755a96c5d337842c9e
dinhdang221101 <82924951+dinhdang221101@users.noreply.github.com>
2025-02-11 21:27:19 +0700

    re

--- src/pages/Users/Confirmation.tsx
+++ src/pages/Users/Confirmation.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../../styles/PaymentResult.css';
+import { useNavigate } from "react-router-dom";
+import "../../styles/PaymentResult.css";
 
 const Confirmation = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleRedirect = () => {
-        navigate('/order-history');
-    };
+  const handleRedirect = () => {
+    navigate("/order-history");
+  };
 
-    return (
-        <div className="result-container">
-            <h1>Đặt hàng thành công!</h1>
-            <p>Cảm ơn bạn đã đặt hàng. Đơn hàng của bạn đã được xử lý.</p>
-            <button onClick={handleRedirect} className="redirect-button">
-                        Xem Lịch Sử Mua Hàng
-                    </button>
-        </div>
-    );
+  return (
+    <div className="result-container">
+      <h1>Đặt hàng thành công!</h1>
+      <p>Cảm ơn bạn đã đặt hàng. Đơn hàng của bạn đã được xử lý.</p>
+      <button onClick={handleRedirect} className="redirect-button">
+        Xem Lịch Sử Mua Hàng
+      </button>
+    </div>
+  );
 };
 
 export default Confirmation;
